58fdd387903abd9641a86aef80141db16cb1b487
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2021-11-06 02:20:24 +0000

    added notes option to form

--- scripts/formScript.js
+++ scripts/formScript.js
@@ -5,8 +5,9 @@ $("#submit").click(function (){
     var dbStatus = document.querySelector('#status').value;
     var dbPlat = document.querySelector('#gplat').value;
     var dbPlan = document.querySelector('#gplan').value;
-    
-    var dataVal = { gameName: dbName , status: dbStatus , gplat: dbPlat , gplan: dbPlan };
+    var dbNote = document.querySelector('#gnote').value;
+
+    var dataVal = { gameName: dbName , status: dbStatus , gplat: dbPlat , gplan: dbPlan , gnote: dbNote };
     console.log(dataVal);
     console.log("data logged");
     window.location.href=window.location.href
