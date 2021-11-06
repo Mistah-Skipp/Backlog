21ff9977db0d62c026b7895957254b92ef921134
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2021-11-06 05:26:48 +0000

    debugging resulted in: data IS being sent and saved correctly BUT not being displayed/printed correctly

--- scripts/page.js
+++ scripts/page.js
@@ -4,8 +4,8 @@ const makeTable = (data) => {
     var tabledata = "<table style='width: 100%'><tr><th style='max-width: 100px'><tr><th>Game</th><th>Status</th><th>Platform</th><th>Plan</th><th>Notes</th></tr>"
 
     data.map((x,id)=> {
-        tabledata += "<tr><td>" + x.gameName + "</td><td>" + x.status + "</td><td>" + x.gplat + "</td><td>" + x.gplan + "</td><td>" + x.gnote + "</td></tr>"
-    })
+        tabledata += "<tr><td>" + x.gameName + "</td><td>" + x.status + "</td><td>" + x.gplat + "</td><td>" + x.gplan + "</td><td>" + x.notes + "</td></tr>"
+    })//FOR DEBUGGING SWAPPED GNOTE FOR ID
 
     tabledata += "</table>"
     document.getElementById("bot").innerHTML = tabledata
@@ -47,14 +47,16 @@ $.put = function (url, data, callback, type) {
 }
 
 $("#submit").click(function() {
+
     var dbName = document.querySelector('#gameName').value;
     var dbStatus = document.querySelector('#status').value;
     var dbPlat = document.querySelector('#gplat').value;
     var dbPlan = document.querySelector('#gplan').value;
     var dbNote = document.querySelector('#gnote').value;
 
-    var dataVal = { gameName: dbName, status: dbStatus, gplat: dbPlat, gplan: dbPlan , gnote: dbNote};
-    console.log(dataVal);
+
+    var dataVal = { title: dbName, status: dbStatus, platform: dbPlat, plan: dbPlan , notes: dbNote};
+    //console.log(dataVal);
     console.log("data logged");
     window.location.href = window.location.href
     $.post("https://backlog-Server.mistahskipp.repl.co/send", dataVal);
