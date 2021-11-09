edeb0de5c75a865bbb59f07fec17e874ec0c101f
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2021-11-09 20:53:11 +0000

    able to add color based on table content, general though and sets it to all with that col. need to add a way to give each type a tag and that gives it the background color (i.e give <sPlat> for switch)

--- scripts/page.js
+++ scripts/page.js
@@ -1,49 +1,27 @@
 console.log("page.js loaded");
-const makeTable = (data) => {
+const sheet = new CSSStyleSheet();
+const makeTable = async = (data) => {
     
-    var tabledata = "<table style='width: 100%'><tr><th style='max-width: 100px'><tr><th>Game</th><th>Status</th><th>Platform</th><th>Plan</th><th>Notes</th></tr>"
-    var debugOut =""
-    data.map((x,id)=> {
-        tabledata += "<tr><td><div id=\"tEdit\">" + x.title + "<\div></td><td><div id=\"sEdit\">" + x.status + "<\div></td><td><div id=\"plEdit\">" + x.platform + "<\div></td><td><div id=\"pnEdit\">" + x.plan + "<\div></td><td><div id=\"nEdit\">" + x.notes + "<\div></td></tr>"
+    var tabledata = "<table id = \"botTable \" style='width: 100%'><tr><th style='max-width: 100px'><tr><th>Game</th><th>Status</th><th>Platform</th><th>Plan</th><th>Notes</th></tr>"
+    data.map((x)=> {
+        if(x.platform == "Switch"){
+            console.log(x.platform);
+            sheet.replaceSync(`.colr #plEdit {background-color: blue;}`);
+            document.adoptedStyleSheets = [sheet];
+        } else if (x.platform == "PC"){
+            console.log(x.platform);
+            sheet.replaceSync(`.colr #plEdit {background-color: red;}`);
+            document.adoptedStyleSheets = [sheet];
+        }
+        tabledata += "<tbody><tr><td class=\"colr\"><div id=\"tEdit\">" + x.title + "<\div></td><td class=\"colr\"><div id=\"sEdit\">" + x.status + "<\div></td><td class=\"colr\"><div id=\"plEdit\">" + x.platform + "<\div></td><td class=\"colr\"><div id=\"pnEdit\">" + x.plan + "<\div></td><td class=\"colr\"><div id=\"nEdit\">" + x.notes + "<\div></td></tr>"
     })
-    tabledata += "</table>"
+    tabledata += "</tbody></table>"
     document.getElementById("bot").innerHTML = tabledata
-}
+}//displaying table from db
 
 var defaultdata;
 fetch('https://backlog-Server.mistahskipp.repl.co/data').then(response => response.json().then(data => {defaultdata = data; makeTable(data)}));
 console.log("Data fetched");
-
-$.delete = function (url, data, callback, type) {
-    if ($.isFunction(data)) {
-        type = type || callback,
-            callback = data,
-            data = {}
-    }
-    return $.ajax({
-        url: url,
-        type: 'DELETE',
-        success: callback,
-        data: data,
-        contentType: type
-    });
-}
-
-$.put = function (url, data, callback, type) {
-    if ($.isFunction(data)) {
-        type = type || callback,
-            callback = data,
-            data = {}
-    }
-
-    return $.ajax({
-        url: url,
-        type: 'PUT',
-        success: callback,
-        data: data,
-        contentType: type
-    });
-}
 
 $("#submit").click(function() {
 
@@ -60,3 +38,8 @@ $("#submit").click(function() {
     window.location.href = window.location.href
     $.post("https://Backlog-Server.mistahskipp.repl.co/send", dataVal);
 })//submit button function
+
+var script = document.createElement('script');
+script.src = "../scripts/color.js";
+script.async = true;
+document.head.appendChild(script);
